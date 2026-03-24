4ae5c9aae8e1c53373dc29c887cb2c3ca67ecb8f
Jorge Cordero <souljah4luv@gmail.com>
2026-03-24 09:05:45 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const cacheName = 'proverbs-cache';
+const assets = [
+  './',
+  './index.html',
+  './styles.css',
+  './proverbs.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
